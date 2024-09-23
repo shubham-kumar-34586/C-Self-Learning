8c54481e2cff4e0b8c9199a9d8a06b9e6e99f847
shubham-kumar-34586 <shubhamkumar34586@gmail.com>
2024-09-23 22:20:45 +0530

    added new file

---
+++ 3_If-else_statement.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*int main(){
+    int age;
+    cout<<"enter your age:-  ";
+    cin>>age;
+    if(age<= 18){
+        cout<<"Not eligible for Driving licence";
+    }
+    else{
+        cout<<"Eigible for Driving licence";
+    }
+}*/
+
+
+
+/*let suppose a school have a grade system 
+like if an student gets 
+<25 consider as A;
+<40 consider as b
+*/
+int main(){
+
+}

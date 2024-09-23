a46ad012dbca518e37a7fab3c801e70613b91a86
shubham-kumar-34586 <shubhamkumar34586@gmail.com>
2024-09-23 22:19:15 +0530

    adding new files

--- 1_myfirst.cpp
+++ 1_myfirst.cpp
@@ -1,7 +1,18 @@
-#include <iostream> //is a header file library that lets us work with input 
+/*#include <iostream> //is a header file library that lets us work with input 
 using namespace std;
 
 int main(){
     cout<< "hello World!!";
     return 0;
-}
+}*/
+
+
+                                        //OR
+
+
+    #include<iostream>
+    
+    int main(){
+        std::cout<<"helllo world";
+        return 0;
+    }
---
+++ 2_Taking_input.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*int main(){
+    int x;
+    cout<<"enter the number";
+    cin>>x;
+    cout<<x;
+}*/
+
+int main(){
+    string x1,x2;
+    cout <<"Enter the charecter name:";
+    cin>>x1;
+    cin>>x2;
+    cout<<"your number is :-"<<x1<<" "<<x2;
+}
+
+/*int main(){
+    int x;
+    int y;
+    cin>>x>>y;
+    cout <<"value of x and y are:- "<< x <<" "<<y;
+
+}
+*/

a290677e5b8bb6c0fef93759dbc51bca8b59a989
shubham-kumar-34586 <shubhamkumar34586@gmail.com>
2024-10-05 03:12:14 +0530

    pattern related problem solving

---
+++ 2_Build_logic/patterns.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   /* for(int row =1;row<=4;row++)
+    {
+        for(int col =1;col<=4;col++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    } 
+
+    output:-
+        * * * * 
+        * * * * 
+        * * * * 
+        * * * *
+
+    
+
+for(int row =1;row<=4;row++)
+    {
+        for(int col =1;col<=5;col++){
+            cout<<"10"<<" ";
+        }
+        cout<<endl;
+    }
+    output:-
+    10 10 10 10 10 
+    10 10 10 10 10 
+    10 10 10 10 10 
+    10 10 10 10 10 
+*/
+/*OR*/
+
+/*for(int row =1;row<=5;row++)
+    {
+        for(int col =1;col<=5;col++){
+            cout<<row<<" ";
+        }
+        cout<<endl;
+    }
+
+    output:-
+    1 1 1 1 1 
+    2 2 2 2 2 
+    3 3 3 3 3 
+    4 4 4 4 4 
+    5 5 5 5 5 */
+
+
+    /*for(int row =1;row<=5;row++)
+    {
+        for(int col =1;col<=5;col++){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    output:-
+        1 2 3 4 5 
+        1 2 3 4 5 
+        1 2 3 4 5 
+        1 2 3 4 5 
+        1 2 3 4 5 */
+
+   /*for(int row =1;row<=5;row++)
+        {
+            for(int col =5;col>=1;col--){
+                cout<<col<<" ";
+            }
+            cout<<endl;
+        }
+        output:-
+        5 4 3 2 1 
+        5 4 3 2 1 
+        5 4 3 2 1 
+        5 4 3 2 1 
+        5 4 3 2 1 */
+/*for(int row =1;row<=5;row++)
+        {
+            for(int col =1;col<=5;col++){
+                cout<<col*col*col<<" ";
+            }
+            cout<<endl;
+        }
+        output:-
+        1 8 27 64 125 
+        1 8 27 64 125 
+        1 8 27 64 125 
+        1 8 27 64 125 
+        1 8 27 64 125 */
+
+/*for(int row =1;row<=5;row++)
+    {
+        char name ='a'+ row-1;
+        for(int col =1;col<=5;col++){
+          cout<<name<<" ";
+            }
+            cout<<endl;
+        }
+        output:-
+        a a a a a 
+        b b b b b 
+        c c c c c 
+        d d d d d 
+        e e e e e */
+
+ /*for(int row =1;row<=5;row++)
+    {
+        for(int col =1;col<=5;col++){
+            char name ='F'+ col-1;
+          cout<<name<<" ";
+            }
+            cout<<endl;
+        }      */ 
+}
